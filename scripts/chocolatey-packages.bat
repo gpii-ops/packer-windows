fe400f04192d9fa1e23547ae82e1f263ff8c441d
Avtar Gill <avtargill@gmail.com>
2016-05-27 21:14:07 -0400

    Updates packages to meet new GPII Windows requirements

--- scripts/chocolatey-packages.bat
+++ scripts/chocolatey-packages.bat
@@ -6,3 +6,8 @@ set choco_binary=c:\programdata\chocolatey\bin\choco.exe
 %choco_binary% install git.install -params '"/GitOnlyOnPath"' -y
 %choco_binary% install firefox -y
 %choco_binary% install googlechrome -y
+%choco_binary% install windows-sdk-8.1 -y
+%choco_binary% install microsoft-build-tools -y
+%choco_binary% install visualcppbuildtools -y
+setx /M VCTargetsPath "C:\Program Files (x86)\MSBuild\Microsoft.Cpp\v4.0\v140"
+
