c43cfe43af1f1c9588d84313db3a27824da20287
Giovanni Tirloni <gpt@gtirloni.com>
2016-08-29 18:17:50 -0300

    Fix build paths.

--- scripts/doit.bat
+++ scripts/doit.bat
@@ -1,17 +1,17 @@
 echo "Building DoIt client for Windows"
 call "C:\Program Files (x86)\Microsoft Visual C++ Build Tools\vcbuildtools_msbuild.bat"
-cd c:\vagrant\doit\client
+cd c:\doit\client
 msbuild doitclient.sln
 copy x64\Release\doitclient.exe C:\Windows
 
 echo "Building DoIt server"
 call "C:\Program Files (x86)\Microsoft Visual C++ Build Tools\vcbuildtools.bat" amd64
-cd c:\vagrant\doit\server
+cd c:\doit\server
 nmake /f Makefile.vc
 copy doit.exe C:\Windows
 
 echo "Copying required files"
-cd c:\vagrant\doit
+cd c:\doit
 copy doit-secret C:\Windows
 copy doitrc C:\Users\vagrant\.doitrc
 copy do.bat C:\Windows
@@ -22,3 +22,6 @@ setx HOME C:\Users\vagrant
 
 echo "Creating DoIt Task (runs on startup)"
 schtasks /create /tn "doit" /tr "C:\Windows\doit.exe C:\Windows\doit-secret" /sc ONSTART /ru vagrant /rp vagrant 
+
+echo "Cleaning up build area"
+rmdir /s /q c:\doit
