5a48a23ab65e8792acea528e85413887ad301ee8
Alfredo Matas <amatas@gmail.com>
2017-03-27 21:11:03 +0200

    Apply the patch for 32 and 64 bits

--- scripts/hotfix-KB3102810.bat
+++ scripts/hotfix-KB3102810.bat
@@ -4,8 +4,16 @@
 
 @echo off
 
+if "%PROCESSOR_ARCHITECTURE%"=="AMD64" goto DL64
+echo "32 bits detected downloading hotfix"
+@powershell -NoProfile -ExecutionPolicy Bypass -Command "((new-object net.webclient).DownloadFile('https://download.microsoft.com/download/A/0/9/A09BC0FD-747C-4B97-8371-1A7F5AC417E9/Windows6.1-KB3102810-x86.msu', 'C:\Windows\Temp\Windows6.1-KB3102810-x64.msu'))"
+goto END
+
+:DL64
+echo "64 bits detected downloading hotfix"
 @powershell -NoProfile -ExecutionPolicy Bypass -Command "((new-object net.webclient).DownloadFile('https://download.microsoft.com/download/F/A/A/FAABD5C2-4600-45F8-96F1-B25B137E3C87/Windows6.1-KB3102810-x64.msu', 'C:\Windows\Temp\Windows6.1-KB3102810-x64.msu'))"
 
+:END
 set hotfix="C:\Windows\Temp\Windows6.1-KB3102810-x64.msu"
 if not exist %hotfix% goto :eof
 
